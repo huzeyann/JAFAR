c0de0cbf21159b617177f7b0a7418388619057ca
Huzheng Yang <huze.yann@gmail.com>
2025-12-04 03:57:02 +0000

    update: create_coordinate function to jafar.py

--- src/upsampler/jafar.py
+++ src/upsampler/jafar.py
@@ -5,7 +5,17 @@ from torch import nn
 
 from src.layers import CrossAttentionBlock, ResBlock, SFTModulation
 from src.layers.positional_encoding import RoPE
-from utils.img import create_coordinate
+
+def create_coordinate(h, w, start=0, end=1, device="cuda", dtype=torch.float32):
+    # Create a grid of coordinates
+    x = torch.linspace(start, end, h, device=device, dtype=dtype)
+    y = torch.linspace(start, end, w, device=device, dtype=dtype)
+    # Create a 2D map using meshgrid
+    xx, yy = torch.meshgrid(x, y, indexing="ij")
+    # Stack the x and y coordinates to create the final map
+    coord_map = torch.stack([xx, yy], axis=-1)[None, ...]
+    coords = rearrange(coord_map, "b h w c -> b (h w) c", h=h, w=w)
+    return coords
 
 
 class JAFAR(nn.Module):
